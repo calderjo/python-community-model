4084be738d27434d68492d3766401d4298816e55
Dhinesh Devanathan <dhineshdevanathan@gmail.com>
2024-03-19 00:06:32 -0400

    Added a basic model file

---
+++ src/my_model/__init__.py
@@ -0,0 +1,11 @@
+import keras
+
+class MyModel(keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.dense1 = keras.layers.Dense(32, activation="relu")
+        self.dense2 = keras.layers.Dense(5, activation="softmax")
+
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        return self.dense2(x)
