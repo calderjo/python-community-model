9fdab347f8304e3db010208543c095259faf4918
Dhinesh Devanathan <dhineshdevanathan@gmail.com>
2024-03-19 00:11:20 -0400

    Added second model

--- src/my_model/__init__.py
+++ src/my_model/__init__.py
@@ -9,3 +9,13 @@ class MyModel(keras.Model):
     def call(self, inputs):
         x = self.dense1(inputs)
         return self.dense2(x)
+    
+class MyModel2(keras.Model):
+    def __init__(self):
+        super().__init__()
+        self.dense1 = keras.layers.Dense(32, activation="relu")
+        self.dense2 = keras.layers.Dense(5, activation="softmax")
+
+    def call(self, inputs):
+        x = self.dense1(inputs)
+        return self.dense2(x)
